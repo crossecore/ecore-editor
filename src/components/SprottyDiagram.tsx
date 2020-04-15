21006057d559631d9483762ce59df268f808189e
Simon Schwichtenberg <schwicht@mail.upb.de>
2020-04-15 12:48:26 +0200

    feat: label width calculation and styles

--- src/components/SprottyDiagram.tsx
+++ src/components/SprottyDiagram.tsx
@@ -14,7 +14,9 @@ import createContainer from '../sprotty-config';
 import ELK, {ElkNode } from 'elkjs/lib/elk.bundled';
 
 
-export default function SprottyDiagram(props:any) {      
+export default function SprottyDiagram(props:any) {
+    let init= false
+    
     // Create Sprotty viewer
     const sprottyContainer = createContainer();
     sprottyContainer.bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope();
@@ -24,8 +26,11 @@ export default function SprottyDiagram(props:any) {
    function updateModel() {
     const elk = new ELK()
     //console.log(elk.knownLayoutOptions())
-    elk.layout(EPackage2ElkGraph.convert(EcorePackageImpl.eINSTANCE), {layoutOptions: { 'algorithm': 'disco' }})
+    const graph = EPackage2ElkGraph.convert(EcorePackageImpl.eINSTANCE)
+
+    elk.layout(graph)
    .then((g:ElkNode) => {
+    console.log(g)
     let sGraph = new ElkGraphJsonToSprotty().transform(g);
     modelSource.updateModel(sGraph)
    })
@@ -33,7 +38,11 @@ export default function SprottyDiagram(props:any) {
 }
 
     useEffect(() => {   
-        updateModel()
+        if(!init){
+            updateModel()
+            init=true
+        }
+        
     });
     return (<div></div>);
     
--- src/elkjs/EPackage2ElkGraph.ts
+++ src/elkjs/EPackage2ElkGraph.ts
@@ -1,9 +1,27 @@
-import { EPackage, EClassImpl, EObject, ENamedElement, EReferenceImpl } from "crossecore";
+import { EPackage, EClassImpl, EObject, ENamedElement, EReferenceImpl, EClass, EClassifier } from "crossecore";
 import { ElkNode, ElkPrimitiveEdge, ElkLabel } from "elkjs";
 
 
 export class EPackage2ElkGraph{
     
+    private static calcWidth = (eClassifier:EClassifier) => {
+        let factor = 50;
+        let indent = 5;
+        let width = eClassifier.name.length*factor
+
+        if(eClassifier instanceof EClassImpl){
+            const eClass = eClassifier as EClass
+            for(const attr of eClass.eAttributes){
+                let current = indent + attr.name.length *factor
+                if(current>width){
+                    width=current
+                }
+            }
+        }
+        console.log(width)
+        return width
+    }
+
     private static getFragment = (eobject:ENamedElement) => {
         let fragment = ""
         let current = eobject
@@ -18,36 +36,36 @@ export class EPackage2ElkGraph{
     }
 
     static convert = (epackage:EPackage)=>{
-
+        const factor = 8
         const classifiers = new Array<ElkNode>()
         const edges = new Array<ElkPrimitiveEdge>()
         for(let eclassifier of epackage.eClassifiers){
-            const labels:Array<ElkLabel> = [{id: Math.random()+"", text: eclassifier.name}]
+            const labels:ElkLabel = {id: Math.random()+"", text: eclassifier.name}
             if(eclassifier instanceof EClassImpl){
                 
                 const eclass = eclassifier as EClassImpl;
                 const features = new Array<ElkNode>()
                 for(let attribute of eclass.eAttributes){
-                    const labels2:Array<ElkLabel> = [{id: Math.random()+"", text: attribute.name+" : " +attribute.eType.name}]
+                    const label:ElkLabel = {id: Math.random()+"", text: attribute.name+" : " +attribute.eType.name}
                     
                     
-                    features.push({id: EPackage2ElkGraph.getFragment(attribute), labels:labels2})
+                    features.push({id: EPackage2ElkGraph.getFragment(attribute), labels:[label], width:label.text.length*factor})
                 }
 
                 for(let reference of eclass.eReferences){
                     const labels2:Array<ElkLabel> = [{id: Math.random()+"", text: reference.name}]
                     edges.push({id: EPackage2ElkGraph.getFragment(reference),source: EPackage2ElkGraph.getFragment(reference.eContainingClass), target: EPackage2ElkGraph.getFragment(reference.eType), labels:labels2})
                 }
-                classifiers.push({id: EPackage2ElkGraph.getFragment(eclass), children: features, width:30, height:30, labels: labels})
+                classifiers.push({id: EPackage2ElkGraph.getFragment(eclass), children: features, width:labels.text.length*factor, height:30, labels: [labels]})
             }
             else{
                 
-                classifiers.push({id: EPackage2ElkGraph.getFragment(eclassifier), width:30, height:30, labels: labels})
+                classifiers.push({id: EPackage2ElkGraph.getFragment(eclassifier), width:labels.text.length*factor, height:30, labels: [labels]})
             }
         }
 
-        const root:ElkNode = {id:EPackage2ElkGraph.getFragment(epackage), children: classifiers, edges: edges}
-        
+        const root:ElkNode = {id:EPackage2ElkGraph.getFragment(epackage), layoutOptions: { 'algorithm': 'stress', "org.eclipse.elk.stress.desiredEdgeLength": "200" }, children: classifiers, edges: edges}
+        console.log(root)
         return root
 
     }
